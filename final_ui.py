6e59cc726e50da0a860d85ce7d5ceb3aa29f4e54
ShounakDeshmukh <shounsach@gmail.com>
2021-10-17 00:21:26 +0530

    changes

--- final_ui.py
+++ final_ui.py
@@ -77,7 +77,7 @@ button_2 = Button(
     image=button_image_2,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_2 clicked"),
+    command=open_text_file,
     relief="flat"
 )
 button_2.place(
